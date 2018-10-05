9564ef9eba058f6a490495ae669119252574b040
drasid <drasidovski@hotmail.com>
2018-10-05 04:13:01 +0000

    updated init.pp with copy index.html

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
 class webserver {
   # this module manage my web server 
   
+  file { "C:\\inetpub\\website":
+    ensure => directory, 
+  }
+
+  file {"C:\\inetpub\\website\\index.html"
+  ensure => file, 
+  source => 'puppet:///modules/webserver/index.html',
+  }
 }
