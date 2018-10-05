1671c1a5aa2a3bc12dfb3ad8443492ee4a30909b
drasid <drasidovski@hotmail.com>
2018-10-05 04:28:58 +0000

    updated to create inetpub directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,15 @@
 class webserver {
   # this module manage my web server 
   
+  file { "C:\\inetpub":
+    ensure => directory, 
+  }
+  
   file { "C:\\inetpub\\website":
     ensure => directory, 
   }
+
+  #$folders = ["C:\\Inetpub",]
 
   file {"C:\\inetpub\\website\\index.html":
   ensure => file, 
